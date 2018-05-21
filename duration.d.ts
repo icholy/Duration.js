0cdf3eb68ffa402b4473d20db803dd4099f43393
Ianko <iankoleite@gmail.com>
2018-05-21 16:07:10 -0400

    Fix the Typescript support.

--- duration.d.ts
+++ duration.d.ts
@@ -1,58 +1,59 @@
-type DurationLike = Duration | string | number;
-type DateLike = Date | number;
+declare module "duration-js" {
+    type DurationLike = Duration | string | number;
+    type DateLike = Date | number;
 
-declare class Duration {
+    export class Duration {
 
-    private _milliseconds: number;
+        private _milliseconds: number;
 
-    constructor(value?: DurationLike);
+        constructor(value?: DurationLike);
 
-    static millisecond: Duration;
-    static second:      Duration;
-    static minute:      Duration;
-    static hour:        Duration;
-    static day:         Duration;
-    static week:        Duration;
+        static millisecond: Duration;
+        static second:      Duration;
+        static minute:      Duration;
+        static hour:        Duration;
+        static day:         Duration;
+        static week:        Duration;
 
-    static milliseconds(milliseconds: number): Duration;
-    static seconds(seconds: number):           Duration;
-    static minutes(minutes: number):           Duration;
-    static hours(hours: number):               Duration;
-    static days(days: number):                 Duration;
-    static weeks(weeks: number):               Duration;
+        static milliseconds(milliseconds: number): Duration;
+        static seconds(seconds: number):           Duration;
+        static minutes(minutes: number):           Duration;
+        static hours(hours: number):               Duration;
+        static days(days: number):                 Duration;
+        static weeks(weeks: number):               Duration;
 
-    nanoseconds():  number;
-    microseconds(): number;
-    milliseconds(): number;
-    seconds():      number;
-    minutes():      number;
-    hours():        number;
-    days():         number;
-    weeks():        number;
+        nanoseconds():  number;
+        microseconds(): number;
+        milliseconds(): number;
+        seconds():      number;
+        minutes():      number;
+        hours():        number;
+        days():         number;
+        weeks():        number;
 
-    toString(): string;
-    valueOf():  number;
+        toString(): string;
+        valueOf():  number;
 
-    isGreaterThan(duration: DurationLike): boolean;
-    isLessThan(duration: DurationLike):    boolean;
-    isEqualTo(duration: DurationLike):     boolean;
+        isGreaterThan(duration: DurationLike): boolean;
+        isLessThan(duration: DurationLike):    boolean;
+        isEqualTo(duration: DurationLike):     boolean;
 
-    roundTo(duration: DurationLike): void;
+        roundTo(duration: DurationLike): void;
 
-    after(date: DateLike): Date;
+        after(date: DateLike): Date;
 
-    static since(date: DateLike):             Duration;
-    static until(date: DateLike):             Duration;
-    static between(a: DateLike, b: DateLike): Duration;
-    static parse(duration: string):           Duration;
-    static fromMicroseconds(us: number):      Duration;
-    static fromNanoseconds(ns: number):       Duration;
+        static since(date: DateLike):             Duration;
+        static until(date: DateLike):             Duration;
+        static between(a: DateLike, b: DateLike): Duration;
+        static parse(duration: string):           Duration;
+        static fromMicroseconds(us: number):      Duration;
+        static fromNanoseconds(ns: number):       Duration;
 
-    static add(a: Duration, b: Duration):      Duration;
-    static subtract(a: Duration, b: Duration): Duration;
-    static multiply(a: Duration, b: number):   Duration;
-    static multiply(a: number, b: Duration):   Duration;
-    static divide(a: Duration, b: Duration):   number;
-    static abs(d: DurationLike):               Duration;
+        static add(a: Duration, b: Duration):      Duration;
+        static subtract(a: Duration, b: Duration): Duration;
+        static multiply(a: Duration, b: number):   Duration;
+        static multiply(a: number, b: Duration):   Duration;
+        static divide(a: Duration, b: Duration):   number;
+        static abs(d: DurationLike):               Duration;
+    }
 }
-
